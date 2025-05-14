be17add0a44db0594229882127da428fa2d7b3e2
Muhammad Ridwan <ridwanrian14789@gmail.com>
2025-05-14 10:02:28 +0700

    feat: add next-sitemap for generating sitemap
    This commit adds the `next-sitemap` package to generate a sitemap for the website.
    
    - Added `next-sitemap` as a dependency.
    - Added a `postbuild` script to run `next-sitemap` after the build process.

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://healtywell.hostspot.online', // GANTI dengan domain kamu
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
