d4210abb1d626834d735a090b9d9941725595baa
Muhammad Ridwan <ridwanrian14789@gmail.com>
2025-06-22 23:46:38 +0700

    feat: remove @cloudflare/next-on-pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Konfigurasi khusus untuk Cloudflare Pages
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
 }
 
 export default nextConfig
